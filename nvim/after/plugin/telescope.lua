bc5070448db44898539f44efb7b949d13bfd073d
Jorge Luis <norestraint@protonmail.com>
2023-01-23 21:54:55 -0300

    initial commit

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,15 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>lg", builtin.live_grep)
+vim.keymap.set("n", "<leader>gs", builtin.grep_string)
+
+vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations)
+vim.keymap.set("n", "<leader>gr", builtin.lsp_references)
+vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions)
+
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+
+-- Project search
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
