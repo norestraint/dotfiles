245f58c05abb05a323de4ccd662aa72e1c0e5eb6
Jorge Luis <norestraint@protonmail.com>
2023-02-02 11:25:07 -0300

    nvim: set the to tokyonight

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -6,7 +6,7 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils("gruvbox")
+ColorMyPencils("tokyonight-night")
 
 -- This needs to be here because it needs to happen after the setting for the theme.
 vim.cmd "highlight SignColumn ctermbg=NONE guibg=NONE"
