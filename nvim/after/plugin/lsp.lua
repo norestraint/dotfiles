5537ffb8dc7c76c4e79c8c1b8b0ce68691d0d80d
Jorge Luis <norestraint@protonmail.com>
2023-04-06 08:31:03 -0300

    nvim[config]: change buffer length

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -141,7 +141,7 @@ local cmp_config = {
   sources = {
     {name = 'path'},
     {name = 'nvim_lsp', keyword_length = 3},
-    {name = 'buffer', keyword_length = 3},
+    {name = 'buffer', keyword_length = 4},
     {name = 'luasnip', keyword_length = 2},
   },
   window = {
