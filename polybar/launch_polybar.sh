0152a5b241886e8da4937c504665361f5ed94930
Jorge Luis <norestraint@protonmail.com>
2023-04-26 13:19:56 -0300

    polybar: initial commit

---
+++ polybar/launch_polybar.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Kill all running instances
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example & disown
+  done
+else
+  polybar --reload example & disown
+fi
