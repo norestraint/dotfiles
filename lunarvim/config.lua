2cf224a55b3a001e88ea84fa35fe70259dca7077
norestraint <norestraint@protonmail.com>
2023-07-10 19:21:31 -0300

    lvim: change colorscheme

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "lunar"
+lvim.colorscheme = "catppuccin-mocha"
 lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
