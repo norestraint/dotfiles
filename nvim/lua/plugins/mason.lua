3b051d33dc957af7a7d33dfecfe2b7383f078d4e
norestrait <norestraint@protonmail.com>
2023-12-20 11:16:03 -0300

    nvim: set mason to autoconfigure new lsp servers

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,18 +1,35 @@
 return {
   "williamboman/mason.nvim",
   cmd = "Mason",
+  dependencies = { "williamboman/mason-lspconfig.nvim" },
   keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-  opts = {
-    ensure_installed = {
-      "gopls",
-      "rust-analyzer",
-    },
-  },
+  opts = {},
+
   ---@param opts MasonSettings | {ensure_installed: string[]}
   config = function(_, opts)
-    require("mason").setup(opts)
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+
+    require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        -- For example, a handler override for the `rust_analyzer`:
+        -- ["rust_analyzer"] = function ()
+        --    require("rust-tools").setup {}
+        -- end
+    }
+
+
     local mr = require("mason-registry")
     local function ensure_installed()
+      if opts.ensure_installed == nil then
+          return
+      end
       for _, tool in ipairs(opts.ensure_installed) do
         local p = mr.get_package(tool)
         if not p:is_installed() then
