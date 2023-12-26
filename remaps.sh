1d0714690caa1ae2a9b1866720fdecc62d67e13f
Jorge Luis <norestraint@protonmail.com>
2023-12-25 22:14:13 -0300

    bash: add cleanup for nixos snapshots

--- remaps.sh
+++ remaps.sh
@@ -2,5 +2,6 @@ alias nv=nvim
 alias nxc='sudo nvim /etc/nixos/configuration.nix'
 
 alias sd="cd \$(find . $HOME | fzf)"
+alias cleanup-nixos='nix-collect-garbage && nix-collect-garbage -d'
 
 eval "$(starship init bash)"
