76af3cc955eaa1996d353d62bc398667d412a3fa
Jorge Luis <norestraint@protonmail.com>
2024-01-03 18:38:20 -0300

    nvim: fix treesitter config to always enable highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,17 @@
 return {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter", 
+    config = function() 
+        require("nvim-treesitter.configs").setup({
+            -- A list of parser names, or "all"
+            ensure_installed = { "vimdoc", "go", "vim", "query", "c", "lua", "rust" },
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+            },
+        })
+    end
 }
