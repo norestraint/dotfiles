d17dd5d58df24823fe272e8a7f2cf8995187c0f0
Jorge Luis <norestraint@protonmail.com>
2024-01-04 15:34:13 -0300

    nvim: cleanup luasnip config

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,16 +1,6 @@
 return {
-    "L3MON4D3/LuaSnip",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    version = "1.2.1",
-    build = "make install_jsregexp",
-    config = function(_)
-        local luasnip = require"luasnip"
-
-        luasnip.config.set_config({
-            region_check_events = 'InsertEnter',
-            delete_check_events = 'InsertLeave'
-        })
-
-        require('luasnip.loaders.from_vscode').lazy_load()
-    end,
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    
 }
