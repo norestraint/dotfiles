65bd0412c75bbcb451d35cf6fa50917e3d307721
Jorge Luis <norestraint@protonmail.com>
2024-05-28 19:05:25 -0300

    add new commands to move around

--- remaps.sh
+++ remaps.sh
@@ -1,7 +1,39 @@
-alias nv=nvim
-alias nxc='sudo nvim /etc/nixos/configuration.nix'
+#!/bin/bash
 
-alias sd="cd \$(find . $HOME | fzf)"
-alias cleanup-nixos='sudo nix-collect-garbage && sudo nix-collect-garbage -d && sudo nixos-rebuild switch'
+editor="lv"
 
-eval "$(starship init bash)"
+ignored_folders=(
+  ".cache" 
+  "target" 
+  ".cargo" 
+  ".config" 
+  ".nvm"
+  ".local"
+  ".git"
+)
+
+go_to_folder="find $HOME -type d \( "
+go_to_file="find $HOME/ -type f \( "
+
+for folder in "${ignored_folders[@]}"; do
+  go_to_folder+="-not -path \"*/$folder*\" -and "
+  go_to_file+="-not -path \"*/$folder*\" -and "
+done
+
+go_to_folder="${go_to_folder::-5}"
+go_to_file="${go_to_file::-5}"
+
+go_to_folder+="\) | fzf"
+go_to_file+="\) | fzf"
+
+alias lv="$HOME/.local/bin/lvim"
+
+# Go to folder
+alias gd='dir=$(eval "$go_to_folder"); if [[ -n "$dir" ]]; then cd "$dir"; fi'
+
+# Load folder into editor
+alias ld='dir=$(eval "$go_to_folder"); if [[ -n "$dir" ]]; then $editor "$dir"; fi'
+
+# Load file into editor
+alias lf='file=$(eval "$go_to_file"); if [[ -n "$file" ]]; then $editor "$file"; fi'
+
