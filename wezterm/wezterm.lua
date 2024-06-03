a4539c8046a03f1f07a2c462e3dcf584cf26b8cb
Jorge Luis <norestraint@protonmail.com>
2024-06-03 01:05:22 -0300

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+return {
+  color_scheme = 'Catppuccin Mocha',
+  enable_tab_bar = false,
+  font_size = 12.0,
+  -- font = wezterm.font('3270SemiCondensed Nerd Font'),
+
+  default_prog = { 'bash' },
+  cursor_blink_rate = 800,
+  window_background_opacity = 1.0,
+  window_decorations = 'RESIZE',
+}
