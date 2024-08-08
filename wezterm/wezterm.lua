0150b4a145064b4efad5bc0ca3c4ca875d047d45
Jorge Luis <norestraint@protonmail.com>
2024-08-08 10:41:36 -0300

    wezterm: refactor config and add correct name for JetBrainsMono

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,15 @@
 local wezterm = require 'wezterm'
-return {
-  color_scheme = 'tokyonight_night',
-  enable_tab_bar = false,
-  font_size = 12.0,
 
-  default_prog = { 'zsh' },
-  cursor_blink_rate = 800,
-  window_background_opacity = 1.0,
-  window_decorations = 'RESIZE',
-}
+local config = wezterm.config_builder()
+
+config.color_scheme = 'tokyonight_night'
+config.enable_tab_bar = false
+config.font_size = 14.0
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+
+config.cursor_blink_rate = 800
+config.default_prog = { 'zsh' }
+config.window_background_opacity = 1.0
+config.window_decorations = 'RESIZE'
+
+return config
