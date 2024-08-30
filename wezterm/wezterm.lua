3ef474128490b36f1e84cda17cd249c6907f49ae
Jorge Luis <norestraint@protonmail.com>
2024-08-30 19:34:27 -0300

    wezterm: change theme and cursor

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,12 +2,18 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'tokyonight_night'
+config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
-config.font_size = 14.0
+config.font_size = 12.0
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 
-config.cursor_blink_rate = 800
+-- config.default_cursor_style = 'BlinkingBlock'
+-- config.default_cursor_style = 'BlinkingUnderline'
+-- config.cursor_blink_rate = 1000
+-- config.cursor_thickness = "200%"
+-- config.cursor_blink_ease_in = "Linear"
+-- config.cursor_blink_ease_out = "Linear"
+
 config.default_prog = { 'zsh' }
 config.window_background_opacity = 1.0
 config.window_decorations = 'RESIZE'
