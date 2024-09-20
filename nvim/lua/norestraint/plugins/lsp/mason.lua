14516fa0abbd5b15d4946497be8146e24b9eb392
Jorge Luis <norestraint@protonmail.com>
2024-09-20 14:56:32 -0300

    nvim: remove unwanted lsp server

--- nvim/lua/norestraint/plugins/lsp/mason.lua
+++ nvim/lua/norestraint/plugins/lsp/mason.lua
@@ -1,51 +1,50 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "bashls",
-        "clangd",
-        "cmake",
-        "cssls",
-        "dockerls",
-        "elixirls",
-        "erlangls",
-        "gopls",
-        "html",
-        "hyprls",
-        "jsonls",
-        "lua_ls",
-        "rust_analyzer",
-        "sqls",
-        "taplo",
-        "ts_ls",
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"bashls",
+				"clangd",
+				"cmake",
+				"cssls",
+				"dockerls",
+				"elixirls",
+				"gopls",
+				"html",
+				"hyprls",
+				"jsonls",
+				"lua_ls",
+				"rust_analyzer",
+				"sqls",
+				"taplo",
+				"ts_ls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "beautysh",
-        "prettier",
-        "stylua",
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"beautysh",
+				"prettier",
+				"stylua",
+			},
+		})
+	end,
 }
