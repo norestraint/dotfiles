d996b4c8ac472bc75d94f0fff76330d555863270
Jorge Luis <norestraint@protonmail.com>
2025-03-06 10:34:56 -0300

    nvim: add nvim-dap.lua

---
+++ nvim/lua/norestraint/plugins/debugger/nvim_dap.lua
@@ -0,0 +1,95 @@
+return {
+	"mfussenegger/nvim-dap",
+	desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui",
+		"mfussenegger/nvim-dap-python",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local dap_python = require("dap-python")
+
+		dapui.setup({})
+		require("nvim-dap-virtual-text").setup({
+			commented = true,
+		})
+
+		dap_python.setup("$HOME/.pyenv/versions/3.11.11/bin/python3.11")
+
+		vim.fn.sign_define("DapBreakpoint", {
+			text = "",
+			texthl = "DiagnosticSignError",
+			linehl = "",
+			numhl = "",
+		})
+
+		vim.fn.sign_define("DapBreakpointRejected", {
+			text = "",
+			texthl = "DiagnosticSignError",
+			linehl = "",
+			numhl = "",
+		})
+
+		vim.fn.sign_define("DapStopped", {
+			text = "",
+			texthl = "DiagnosticSignWarn",
+			linehl = "Visual",
+			numhl = "DiagnosticSignWarn",
+		})
+
+		-- Automatically open/close DAP UI
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+
+		local function opts(desc)
+			return {
+				noremap = true,
+				silent = true,
+				desc = desc,
+			}
+		end
+
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>db", function()
+			dap.toggle_breakpoint()
+		end, opts("Toggle breakpoint"))
+
+		keymap.set("n", "<leader>dc", function()
+			dap.continue()
+		end, opts("Continue/Start"))
+
+		keymap.set("n", "<leader>do", function()
+			dap.step_over()
+		end, opts("Step over"))
+
+		keymap.set("n", "<leader>di", function()
+			dap.step_into()
+		end, opts("Step into"))
+
+		keymap.set("n", "<leader>dO", function()
+			dap.step_out()
+		end, opts("Step out"))
+
+		keymap.set("n", "<leader>dq", function()
+			dap.terminate()
+		end, opts("Terminate debugging."))
+
+		keymap.set("n", "<leader>du", function()
+			dapui.toggle()
+		end, opts("Toggle DAP UI"))
+	end,
+}
