a8a0e0a1cbc1e430a896cfb7aaee2ed5002837bf
Jorge Luis <norestraint@protonmail.com>
2025-03-08 22:30:29 -0300

    nvim: fix bufferline bg color

--- nvim/lua/norestraint/plugins/bufferline.lua
+++ nvim/lua/norestraint/plugins/bufferline.lua
@@ -1,10 +1,149 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			indicator = {
+				style = "icon",
+				icon = "▎",
+			},
+			diagnostics = "nvim_lsp",
+			auto_toggle_bufferline = true,
+			mode = "buffers",
+		},
+		highlights = {
+			fill = {
+				fg = "#1e1e2e",
+				bg = "#1e1e2e",
+			},
+			background = {
+				bg = "#1e1e2e",
+			},
+			buffer_visible = {
+				bg = "#1e1e2e",
+			},
+			buffer_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			close_button = {
+				bg = "#1e1e2e",
+			},
+			close_button_visible = {
+				bg = "#1e1e2e",
+			},
+			close_button_selected = {
+				bg = "#1e1e2e",
+				fg = "#f38ba8",
+			},
+			diagnostic = {
+				bg = "#1e1e2e",
+			},
+			diagnostic_visible = {
+				bg = "#1e1e2e",
+			},
+			diagnostic_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			separator_selected = {
+				bg = "#1e1e2e",
+			},
+			separator_visible = {
+				bg = "#1e1e2e",
+				fg = "#45475a",
+			},
+			separator = {
+				bg = "#1e1e2e",
+				fg = "#45475a",
+			},
+			indicator_visible = {
+				bg = "#1e1e2e",
+			},
+			indicator_selected = {
+				bg = "#1e1e2e",
+				fg = "#cba6f7",
+			},
+			error = {
+				bg = "#1e1e2e",
+			},
+			error_visible = {
+				bg = "#1e1e2e",
+			},
+			error_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			hint = {
+				bg = "#1e1e2e",
+			},
+			hint_visible = {
+				bg = "#1e1e2e",
+			},
+			hint_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			hint_diagnostic = {
+				bg = "#1e1e2e",
+			},
+			hint_diagnostic_visible = {
+				bg = "#1e1e2e",
+			},
+			hint_diagnostic_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			info = {
+				bg = "#1e1e2e",
+			},
+			info_visible = {
+				bg = "#1e1e2e",
+			},
+			info_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			info_diagnostic = {
+				bg = "#1e1e2e",
+			},
+			info_diagnostic_visible = {
+				bg = "#1e1e2e",
+			},
+			info_diagnostic_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			warning = {
+				bg = "#1e1e2e",
+			},
+			warning_visible = {
+				bg = "#1e1e2e",
+			},
+			warning_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+			warning_diagnostic = {
+				bg = "#1e1e2e",
+			},
+			warning_diagnostic_visible = {
+				bg = "#1e1e2e",
+			},
+			warning_diagnostic_selected = {
+				bg = "#1e1e2e",
+				bold = true,
+				italic = true,
+			},
+		},
+	},
 }
