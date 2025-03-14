58debaa57cfa7a319b37e51635e01861934267eb
Jorge Luis <norestraint@protonmail.com>
2025-03-14 14:42:38 -0300

    nvim: fix some keymaps for blink

--- nvim/lua/norestraint/plugins/blink.lua
+++ nvim/lua/norestraint/plugins/blink.lua
@@ -24,14 +24,39 @@ return {
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
 		},
 		appearance = {
-			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
 		},
 		sources = {
-			default = { "lsp", "snippets", "path", "buffer" },
+			default = { "snippets", "lsp", "path", "buffer" },
 		},
 
 		fuzzy = { implementation = "prefer_rust_with_warning" },
+		cmdline = {
+			completion = {
+				menu = {
+					auto_show = true,
+				},
+				ghost_text = {
+					enabled = false,
+				},
+			},
+			keymap = {
+				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide" },
+				["<C-y>"] = { "select_and_accept" },
+
+				["<C-k>"] = { "select_prev", "fallback" },
+				["<C-j>"] = { "select_next", "fallback" },
+				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+				["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+			},
+		},
 	},
 	opts_extend = { "sources.default" },
 }
