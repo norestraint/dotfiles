98ba383451fa297fc70c0a930065a26cdc6fc8c9
Jorge Luis <norestraint@protonmail.com>
2025-03-18 10:03:36 -0300

    nvim: show hidden files on Oil

--- nvim/lua/norestraint/plugins/oil.lua
+++ nvim/lua/norestraint/plugins/oil.lua
@@ -5,4 +5,7 @@ return {
 	opts = {},
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	lazy = false,
+	config = function()
+		require("oil").setup({ view_options = { show_hidden = true } })
+	end,
 }
