9eee3e8843df38e70053755f2c9fa9c403edd876
Jorge Luis <norestraint@protonmail.com>
2025-05-01 13:20:57 -0300

    zsh: fix and improve aliases and add init for conda

--- remaps.sh
+++ remaps.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
-
 ignored_folders=(
-  ".cache" 
-  "target" 
-  ".cargo" 
-  ".config" 
+  ".cache"
+  ".asdf"
+  ".pyenv"
+  ".bun"
+  "deps"
+  "target"
+  ".cargo"
+  ".config"
   ".nvm"
   ".local"
   ".git"
   "node_modules"
   "go"
   ".rustup"
+  ".var"
+  "obsidian"
+  ".logseq"
+  ".mozilla"
+  "miniconda3"
 )
 
-go_to_folder="find $HOME -type d \( "
-go_to_file="find $HOME/ -type f \( "
+# Start building the find command with prune expressions
+go_to_folder="find $HOME"
+go_to_file="find $HOME"
 
+# Add prune expressions for each ignored folder
 for folder in "${ignored_folders[@]}"; do
-  go_to_folder+="-not -path \"*/$folder*\" -and "
-  go_to_file+="-not -path \"*/$folder*\" -and "
+  go_to_folder="$go_to_folder -path '*/$folder' -prune -o -path '*/$folder/*' -prune -o"
+  go_to_file="$go_to_file -path '*/$folder' -prune -o -path '*/$folder/*' -prune -o"
 done
 
-go_to_folder="${go_to_folder::-5}"
-go_to_file="${go_to_file::-5}"
-
-go_to_folder+="\) | fzf"
-go_to_file+="\) | fzf"
-
-alias lv="$HOME/.local/bin/lvim"
-alias wezterm='flatpak run org.wezfurlong.wezterm'
+# Complete the commands with type conditions
+go_to_folder="$go_to_folder -type d -print | fzf"
+go_to_file="$go_to_file -type f -print | fzf"
 
 # Go to folder
 alias gd='dir=$(eval "$go_to_folder"); if [[ -n "$dir" ]]; then cd "$dir"; fi'
-
 # Load folder into editor
-alias ld='dir=$(eval "$go_to_folder"); if [[ -n "$dir" ]]; then cd "$dir" && lv . ; fi'
-
+alias ld='dir=$(eval "$go_to_folder"); if [[ -n "$dir" ]]; then cd "$dir" && nv . ; fi'
 # Load file into editor
-alias lf='file=$(eval "$go_to_file"); if [[ -n "$file" ]]; then lv "$file"; fi'
+alias lf='file=$(eval "$go_to_file"); if [[ -n "$file" ]]; then nv "$file"; fi'
+
+alias wezterm='flatpak run org.wezfurlong.wezterm'
 
 # Enables history on the Elixir's iex environment
 export ERL_AFLAGS="-kernel shell_history enabled"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -81,9 +81,7 @@ setopt hist_find_no_dups
 # Aliases
 
 # Load files/directories with neovim.
-alias lf='nvim $(find $HOME -type f | fzf -m --no-ignore-case --preview="bat --color=always {}" --tmux)'
-alias ld='cd $(find $HOME -type d | fzf --no-ignore-case -m --tmux) && nvim .'
-alias gd='cd $(find $HOME -type d | fzf --no-ignore-case -m --tmux)'
+source "$HOME/dotfiles/remaps.sh"
 
 # Load pdfs with zathura.
 alias rb='zathura $(find $HOME/Documents/books | fzf --tmux) &'
@@ -149,3 +147,19 @@ eval "$(pyenv init - zsh)"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+# __conda_setup="$('/home/norestraint/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     eval "$__conda_setup"
+# else
+#     if [ -f "/home/norestraint/miniconda3/etc/profile.d/conda.sh" ]; then
+#         . "/home/norestraint/miniconda3/etc/profile.d/conda.sh"
+#     else
+#         export PATH="/home/norestraint/miniconda3/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
+# <<< conda initialize <<<
+
