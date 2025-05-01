12b57c5fea79a8a5300ff937862129b2abf9e7cf
Jorge Luis <norestraint@protonmail.com>
2025-05-01 13:19:08 -0300

    nvim: add highlight for yanks

--- nvim/lua/norestraint/core/init.lua
+++ nvim/lua/norestraint/core/init.lua
@@ -1,2 +1,10 @@
 require("norestraint.core.options")
 require("norestraint.core.keymaps")
+
+-- Highlight yanked
+vim.cmd([[
+  augroup highlight_yank
+      autocmd!
+      au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=200})
+  augroup END
+]])
