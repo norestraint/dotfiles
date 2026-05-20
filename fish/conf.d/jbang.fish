65d2e00efb6bd5beef78acb6595d389d8d04e7a3
Jorge Luis R. R. Junior <norestraint@protonmail.com>
2026-05-20 00:31:39 -0300

    fish: some updates

---
+++ fish/conf.d/jbang.fish
@@ -0,0 +1,6 @@
+# Add JBang to environment
+
+abbr --add j! jbang
+
+fish_add_path /home/norestraint/.jbang/bin
+
--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,19 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
 set EDITOR nvim
 
 starship init fish | source
 
 zoxide init fish | source
 
-fish_add_path -g "$HOME/bin"
+# fish_add_path -g "$HOME/bin"
 
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
+~/.local/bin/mise activate fish | source
+
+source "$HOME/.cargo/env.fish"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
